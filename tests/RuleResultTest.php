aa329503cc671509df40a0b155843ecdcdc47d2a
pengboomouch <pengboomouch@gmail.com>
2023-02-22 17:10:28 +0100

    tests fixed

--- tests/RuleResultTest.php
+++ tests/RuleResultTest.php
@@ -7,7 +7,7 @@ class RuleResultTest extends \PHPUnit\Framework\TestCase
     {
         $ruleResult = $this->createStub(\Regulus\RuleResult::class);
         $ruleResult->method('getRuleName')->willReturn('test');
-        $ruleResult->method('getConditionNames')->willReturn(['condition1, condition2']);
+        $ruleResult->method('getConditionNames')->willReturn(["condition1", "condition2"]);
 
         $this->assertEquals('test', $ruleResult->getRuleName());
         $this->assertTrue(count($ruleResult->getConditionNames()) === 2);
